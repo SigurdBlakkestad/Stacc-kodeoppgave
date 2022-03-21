594aba6a473ee737ad06039d35c28ea81b51e72e
Sigurd.Blakkestad <sigurd.blakkestad@student.uib.no>
2022-03-21 23:19:07 +0100

    search bar

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input placeholder="Enter Post Title"/>
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
