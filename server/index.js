cbad921f89137736378a092338d3f599826ef8a7
Sigurd.Blakkestad <sigurd.blakkestad@student.uib.no>
2022-03-21 18:18:36 +0100

    begynt å gjøre klar til å lese inn csv

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@
 
 const path = require('path');
 const express = require("express");
+const fs = require('fs'); 
+const parse = require('csv-parse');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../webapp/build')));
+app.use(express.json());
 
 
 app.get("/api", (req, res) => {
