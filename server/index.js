bd5279f9df6066a3ab258a9deb53d898d3e2f69f
Sigurd.Blakkestad <sigurd.blakkestad@student.uib.no>
2022-04-23 14:49:08 +0200

    laget mvp

--- server/index.js
+++ server/index.js
@@ -1,27 +1,47 @@
 // server/index.js
 
 const path = require('path');
-const express = require("express");
-const fs = require('fs'); 
-//const csv = require('csv-parse');
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+var cors = require('cors');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, '../webapp/build')));
 app.use(express.json());
 
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Ta en pep sjekk da vell!" });
+//Searches through a local csv file and outputs whether or not a person is politically exposed or not
+app.get('/api/pep', (req, res) => {
+    const name = req.query.name;
+    var numberOfHits = 0;
+    var results = [];
+
+    fs.createReadStream(path.resolve(__dirname, 'pep.csv'))
+    .pipe(csv())
+    .on('data', (row) => {
+      if(typeof row.name != 'undefined' && row.name.toLowerCase() == name.toLowerCase()){
+        numberOfHits++;
+        results.push(row);
+      }
+    }).on('end', () => {
+      res.status(200).send({
+        "numberOfHits": numberOfHits,
+        "results": results
+      });
+    });
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../webapp/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../webapp/build', 'index.html'));
 });
-  
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+
+app.listen(
+  PORT,
+  () => console.log(`server is alive on http://localhost:${PORT}`)
+)
 
 //dette kan kjøres ved å skrive dette i din valgte nettleser: http://localhost:3001/api eller på https://stacc-kodeoppgave-22-sb.herokuapp.com/
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,18 +5,30 @@ import './App.css';
 function App() {
   const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  const searchPerson = (name) => {
+    let url = "/api/pep?name=" + name;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => setData(data));
+  }
+
+  var userInput = React.createRef();
+
+  const handleClick = () => { searchPerson(userInput.current.value); }
 
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Search for politically exposed people</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-        <input placeholder="Skriv inn et navn her"/>
+        <div>
+        <input ref={userInput}></input>
+        <button onClick={handleClick}>Search</button>
+        </div>
+        <div>
+        <p>{!data ? "": "Number of results: " + data.numberOfHits}</p>
+        <p>{!data ? "": !data.numberOfHits ? "No match found" : data.results[0].name + " is a politically exposed person"}</p>
+        </div>
       </header>
     </div>
   );
